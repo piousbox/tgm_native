74b00acef9b5c160fba272be322e42074c09089f
v p <piousbox@vs-MacBook-Pro.local>
2017-12-27 20:02:07 -0800

    wiring redux there

--- my-app/App.js
+++ my-app/App.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { Actions, Scene, Stack, Router } from 'react-native-router-flux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+const siteReducer = (state={}, action) => {
+  return state
+}
+const reducers = {
+  site: siteReducer,
+}
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+const reducer = combineReducers(reducers)
+const store = createStoreWithMiddleware(reducer)
+
+class Home extends React.Component {
+  render () {
+    return (<View style={styles.container}><Text>This is home</Text></View>)
+  }
+}
+
+class Home2 extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text>I edited this Five times. Open up App.js to</Text>
+        <Text>start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList data={ [{key: 'a'}, {key:'b'}] }
+                  renderItem={({item}) => <Text>{item.key}</Text>}
+        />
+      </View>)
+  }
+}
+
+const scenes = Actions.create(
+  <Scene key='root'>
+    <Scene key='home' component={Home} />
+    <Scene key='home2' component={Home2} />
+  </Scene>)
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>I edited this four times. Open up App.js to</Text>
-        <Text>start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Provider store={store}>
+        <Router scenes={scenes} />
+      </Provider>
+    )
   }
 }
 
@@ -21,4 +59,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
